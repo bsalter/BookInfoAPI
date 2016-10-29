ff70d8535b88d4998ecad229c3d1ada083e3462e
Benn Salter <bennsalter@gmail.com>
2016-10-29 09:40:06 -0400

    mongo setup

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var fs = require('fs');
 
 var app = express();
 
+var db;
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+MongoClient.connect(process.env.HEROKU_MONGO, function (err, database) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    db = database;
+    database.close();
+});
+
 app.use(function(req, res, next){
     console.log((new Date()).toString() + " " + req.method + " " + req.url + " " + res.statusCode);
     next();
@@ -19,18 +30,18 @@ app.get('/', function(req, res){
 
 app.get('/reactions', function(req, res){
     var query = req.query.q;
-/*
     if(typeof query == "undefined") {
-        var errorobj = {"status":414,"message":"No query parameter provided"};
-        res.status(414).json(errorobj);
-    } else if(query.length > 1000) {
-        var errorobj = {"status":414,"message":"Text too long"};
+        var errorobj = {"status": 414, "message": "No query parameter provided"};
         res.status(414).json(errorobj);
     } else {
-        var resobj = {"result": this_translator.translateToEnglish(query).translation};
+        var resobj = {"status":200, "result": this_translator.translateToEnglish(query).translation};
         res.json(resobj);
     }
-*/
+});
+
+app.post('/book', function(req, rest) {
+    var query = req.query.q;
+    res.status(200);
 });
 
 app.use(function(req, res) {
