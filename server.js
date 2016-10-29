d9503575891b895701f6d0f1e205be7c4a8bd6ef
Benn Salter <bennsalter@gmail.com>
2016-10-29 11:02:26 -0400

    endpoint work

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var db;
 var MongoClient = require('mongodb').MongoClient
@@ -18,26 +23,37 @@ app.use(function(req, res, next){
     next();
 });
 
-app.get('/reactions', function(req, res){
+app.get('/book', function(req, res){
     var query = req.query.book;
     if(typeof query == "undefined") {
-        var errorobj = {"status": 414, "message": "No query parameter provided"};
-        res.status(414).json(errorobj);
+        var errorobj = {"status": 404, "message": "No query parameter provided"};
+        res.status(404).json(errorobj);
     } else {
         var resobj = {"status":200, "result": ""};
         res.json(resobj);
     }
 });
 
+app.post('/reaction', function(req, res) {
+    var query = req.body;
+    if(typeof query == "undefined") {
+        var errorobj = {"status": 404, "message": "No data provided"};
+        res.status(404).json(errorobj);
+    } else {
+        var book = query.book;
+        var title = book.title;
+        var reaction = book.reaction;
+        var time = book.timestamp;
+        var user = book.user;
+        res.status(200).json(book);
+    }
+});
+
+/*
 app.get('/timeframe', function(req, res) {
     var query = req.query.request;
-
 });
-
-app.post('/book', function(req, res) {
-    var query = req.query.book;
-    res.status(200);
-});
+*/
 
 app.use(function(req, res) {
     res.status(404);
